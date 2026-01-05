70e52255e9b3e458dbabea460940fc249aa4585d
Francisco Rodriguez <chillguy27@outlook.com>
2026-01-05 13:55:17 -0600

    Refactor app configuration and routing; implement counter and hero pages
    - Simplified app configuration by consolidating provider array.
    - Removed placeholder content from app.html and added a section for router outlet.
    - Defined routes for CounterPageComponent and HeroPageComponent.
    - Created CounterPageComponent with functionality to increase, decrease, and reset a counter.
    - Developed HeroPageComponent to display and manipulate hero information using signals.
    - Added styles for hero page elements.
    - Integrated Bootstrap for improved styling in index.html.

---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,41 @@
+import { Component, computed, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  imports: [UpperCasePipe],
+  styleUrl: './hero-styles.css',
+  templateUrl: './hero-page.component.html',
+})
+export class HeroPageComponent {
+  nameSignal = signal('Iron Man');
+  ageSignal = signal(45);
+
+  heroDescription = computed(() => {
+    const description = `${this.nameSignal()} - ${this.ageSignal()}`;
+
+    return description;
+  });
+
+  capitalizedName = computed(() => {
+    return this.nameSignal().toUpperCase();
+  });
+
+  // getHeroDescription() {
+  //   return `${this.nameSignal()} - ${this.ageSignal()}`;
+  // }
+
+  changeHero() {
+    this.nameSignal.set('Spiderman');
+    this.ageSignal.set(22);
+  }
+
+  resetForm() {
+    this.nameSignal.update((current) => (current = 'Iron Man'));
+    this.nameSignal.set('Iron Man');
+    this.ageSignal.set(45);
+  }
+
+  changeAgeTo(value: number) {
+    this.ageSignal.set(value);
+  }
+}
